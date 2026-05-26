f035209be3b2887c4324a2700885383f4c80c1ed
madhumithaita <madhumitha.it22@bitsathy.ac.in>
2026-05-26 14:54:16 +0530

    acmejc_6-removed duplicate imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrchastratorModule } from './orchastrator/orchastrator.module';
-import { ChunkProducerModule } from './chunk_producer/chunk_producer.module';
-import { ChunkConsumerModule } from './chunk_consumer/chunk_consumer.module';
 import { MergerModule } from './merger/merger.module';
 import { ChunkProducerModule } from './chunk-producer/chunk-producer.module';
 import { ChunkConsumerModule } from './chunk-consumer/chunk-consumer.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [OrchastratorModule, ChunkProducerModule, ChunkConsumerModule, MergerModule],
+  imports: [OrchastratorModule, ChunkProducerModule, ChunkConsumerModule, MergerModule,ConfigModule.forRoot({
+      isGlobal:true})
+],
   controllers: [AppController],
   providers: [AppService],
 })
