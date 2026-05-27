7693066b76f6014c83b6c2b79a937b7ff1180aa1
muhammedarsathcn <muhammed.arsathcn@cdw.com>
2026-05-27 15:55:05 +0530

    ACMEJS-11 refactor: commitlint regex pattern in variable

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,27 +1,26 @@
-// commit msg format : ACMEJS-(TICKET NUMBER) type : msg
+// commit msg format : ACMEJS-(TICKET NUMBER) type: msg
+
+const COMMIT_TYPES = 'feat|fix|chore|docs|refactor|test|add|merge|remove|perf|ci';
 
 export default {
-  extends: ['@commitlint/config-conventional'],
+    extends: ['@commitlint/config-conventional'],
 
-  parserPreset: {
-    parserOpts: {
-      headerPattern:
-        /^([A-Z]+-\d+)\s(feat|fix|chore|docs|refactor|test|add|merge|remove|perf|ci):\s(.+)$/,
-      headerCorrespondence: ['ticket', 'type', 'subject'],
+    parserPreset: {
+        parserOpts: {
+            headerPattern: new RegExp(`^([A-Z]+-\\d+)\\s(${COMMIT_TYPES}):\\s(.+)$`),
+            headerCorrespondence: ['ticket', 'type', 'subject'],
+        },
     },
-  },
 
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'remove', 'docs', 'refactor', 'test', 'perf', 'ci', 'add', 'merge'],
-    ],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            ['feat', 'fix', 'remove', 'docs', 'refactor', 'test', 'perf', 'ci', 'add', 'merge'],
+        ],
 
-    'type-empty': [2, 'never'],
-
-    'subject-empty': [2, 'never'],
-
-    'header-max-length': [2, 'always', 100],
-  },
+        'type-empty': [2, 'never'],
+        'subject-empty': [2, 'never'],
+        'header-max-length': [2, 'always', 100],
+    },
 };
